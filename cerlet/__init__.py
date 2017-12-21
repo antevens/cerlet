a9e282c36b70ec4ef4ca84b6a718e6132fcfa49c
Antonia Stevens <a@antevens.com>
2017-12-21 21:49:48 +0000

    Cleanup

--- cerlet/__init__.py
+++ cerlet/__init__.py
@@ -24,17 +24,12 @@ logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
 
 
-default_ca_path = '/tmp/ca.pem'
-default_ca_file = '/tmp/ca_files'
-default_keytab_path = '/tmp/keytab'
-default_submitter_principal = 'hostname'
-default_request_profile = None
-default_request_principal = 'hostname'
-
-
 def main():
     """ Main entry point to program """
+    # Parse command line arguments
     args = parse_args()
+
+    # Set up IPA API connection
     ipalib.api.bootstrap_with_global_options(context='cerlet')
     ipalib.api.finalize()
 
@@ -43,7 +38,7 @@ def main():
     else:
         ipalib.api.Backend.rpcclient.connect()
 
-
+    # Read CSR File and extract/get relevant data
     with open(args.csr_file, 'rb') as f:
         der_bytes = f.read()
         if asn1crypto.pem.detect(der_bytes):
@@ -56,10 +51,12 @@ def main():
     host = ipalib.api.Command.host_show(common_name)['result']
     principals = host['krbprincipalname']
     fqdn = host['fqdn']
-    # Host names and IP addresses
-    subjects = [match.group(1) for principal in principals for match in
-            [FQDN_PATTERN.match(principal), IPADDRESS_PATTERN.match(principal)] if match]
+    # Create unique list of FQDNs from principals and hostname (CSR Subject)
+    subjects = set(list(fqdn) + [match.group(1) for principal in principals for match in
+            [FQDN_PATTERN.match(principal), IPADDRESS_PATTERN.match(principal)]
+            if match])
 
+    # Find the DNS Zone to add/modify records to/in
     for subject_alt_name in subjects:
         dns_zone_candidate = dns.name.from_text(subject_alt_name)
         while True:
@@ -72,6 +69,7 @@ def main():
                 except dns.name.NoParent:
                     raise ipalib.errors.NotFound('Unable to find DNS Zone on IPA server')
 
+        # Add/Modify DNS records used for authorization by Let's Encrypt
         acme_record = "_acme-challenge.{0}.".format(subject_alt_name)
         challenge = "bogus_challenge"
         try:
@@ -107,37 +105,27 @@ def generate_ipa_xml_rpc_url():
 def parse_args():
     parser = argparse.ArgumentParser(description="Apply for Let's Encrypt Certificates", add_help=False)
     parser.add_argument('-h', '--host_name', '--host', dest='ipa_host',
-                        default=lookup_ipa_host(),
                         help='Hostname or IP Address of the IPA (Satellite) server')
     parser.add_argument('-d', '--domain_name', '--domain', dest='ipa_domain',
-                        default=lookup_ipa_domain(),
                         help='Domain in IPA (Satellite) to register under')
     parser.add_argument('-H', '--xml_rpc_url', '--xmlrpc-url', dest='ipa_xml_rpc_url',
-                        default=generate_ipa_xml_rpc_url(),
                         help='XML RPC service location')
     parser.add_argument('-C', '--ca_path', '--capath', dest='ca_path',
-                        default=default_ca_path,
                         help='Path do a directory containing PEM encoded CA files')
     parser.add_argument('-c', '--ca_file', '--cafile', dest='ca_file',
-                        default=default_ca_file,
                         help='Path do a file containing PEM encoded CA certificates')
     parser.add_argument('-t', '--keytab_name', '--keytab-name', dest='keytab_file',
-                        default=default_keytab_path,
                         help='Path to a keytab file containing credentials for IPA server authentication')
     parser.add_argument('-k', '--submitter_principal', '--submitter-principal', dest='submitter_principal',
-                        default=default_submitter_principal,
                         help='Kerberos principal for IPA server authentication')
     parser.add_argument('-K', '--use_ccache_creds', '--use-ccache-creds', dest='use_ccache_creds',
                         default=False, action='store_true',
                         help='Use default ccache for authorization instead of authenticating')
     parser.add_argument('-P', '--request_principal', '--principal-of-request', dest='request_principal',
-                        default=default_request_principal,
                         help='Principal(s) (FQDN) used in signing request, comma separated')
     parser.add_argument('-T', '--request_profile', '--profile', dest='request_profile',
-                        default=default_request_profile,
                         help='Use a specific profile when requesting enrollment')
-    parser.add_argument('-v', '--verbose', dest='verbose',
-                        default=False, action='store_true',
+    parser.add_argument('-v', '--verbose', dest='verbose', default=False, action='store_true',
                         help='Use verbose logging')
     parser.add_argument('--help', action='help', help='Show this help message and exit')
     parser.add_argument('options', help='Options')
